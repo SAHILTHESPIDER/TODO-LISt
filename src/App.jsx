2f394b1c3ace11f32afd62e6f8785f30d04c8b3e
SAHILTHESPIDER <sahilgupta9594@gmail.com>
2024-07-24 23:05:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Todoinput from './component/Todoinput';
+function App() {
+
+  return (
+    <>
+     <Todoinput></Todoinput>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Todoinput.jsx
@@ -0,0 +1,12 @@
+function Todoinput(){
+   return(
+      <>
+      <div className="main-container">
+         <h1> LIST</h1>
+      <input type="text" placeholder="Enter your List" />
+      </div>
+      </>
+     
+   );
+}
+export default Todoinput;
