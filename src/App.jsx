04ed826d8f6f63c3976a767a7bf837d59d1edb44
SAHILTHESPIDER <sahilgupta9594@gmail.com>
2024-07-27 21:52:22 +0530

    2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import './App.css';
-import Todoinput from './component/Todoinput';
+
+import AppName from './component/AppName';
+import AddTodo from './component/AddTodo'; 
+import TodoItem from './component/TodoItem'; 
+import TodoItems from './component/TodoItems';
+import WelcomeMessage from './component/Welcome';
+import "./style.css";
+import { useState } from "react";
+
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
 
   return (
-    <>
-     <Todoinput></Todoinput>
-    </>
-  )
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
+    </center>
+  );
 }
 
-export default App
+export default App;
